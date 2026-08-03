35c652485c5ebf78bebf555dbfa274486ac37c49
Hendrik Minniear Rombach <h.minniear@icloud.com>
2026-08-03 23:42:03 +0200

    Initial Commit

---
+++ CarAOD/CarAODApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarAODApp.swift
+//  CarAOD
+//
+//  Created by Hendrik Rombach on 03.08.26.
+//
+
+import SwiftUI
+
+@main
+struct CarAODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
